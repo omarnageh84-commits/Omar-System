17c51b76e402d98371be48ff3401f3453f8b1473
omarnageh84-commits <omarnageh84@gmail.com>
2026-07-31 11:20:57 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 const firebaseConfig = {
   apiKey: "AIzaSyAUuFRP12_gEp8WjVH582rOvX5JrBk4DA",
   authDomain: "omar-system-2026.firebaseapp.com",
@@ -9,14 +8,10 @@ const firebaseConfig = {
   messagingSenderId: "136389596066",
   appId: "1:136389596066:web:e8585b28fd81db06e818fb"
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const USER_ID = "omar_main";
-
-// اللينك الجديد النهائي - الإصدار 17
 export const GOOGLE_SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbx7rPrkIqZOR-Q22nRRoouUrbJnNiQ92za17Zw3QF5m-Y12efdy0aep8D_LgkesJdPQ2g/exec";
-
 export async function saveToFirebase(storeName, data) {
   const ref = doc(db, "users", USER_ID);
   await setDoc(ref, { [storeName]: data }, { merge: true });
