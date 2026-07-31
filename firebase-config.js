87ac472df3a1fa55fefca818baf186f00e1565ce
omarnageh84-commits <omarnageh84@gmail.com>
2026-07-31 10:53:34 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAUuFRP12_gEp8WjVH582rOvX5JrBk4DA",
   authDomain: "omar-system-2026.firebaseapp.com",
@@ -8,6 +9,10 @@ const firebaseConfig = {
   messagingSenderId: "136389596066",
   appId: "1:136389596066:web:e8585b28fd81db06e818fb"
 };
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const USER_ID = "omar_main";
+
+// ده لينك الشيت الجديد بتاعك - المراية اللي بتكمل
+export const GOOGLE_SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbymAX_drOLObItZtYvQlv2DwiTwUp8gDrPrtQyNyLUZVTUH6GTJINe2xnIUYvFwEIHlcw/exec";
